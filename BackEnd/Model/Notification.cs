0cc0f3899b275e422bd2c225ff3c24a1a369c0f3
bshively2 <97967055+bshively2@users.noreply.github.com>
2022-03-09 16:12:47 -0500

    Delete BackEnd directory

--- BackEnd/Model/Notification.cs
+++
@@ -1,9 +0,0 @@
-namespace Model
-{
-    public class Notification
-    {
-        public int customerId { get; set; }
-        public string cryptoName { get; set; }
-        public decimal alertPrice { get; set; }
-    }
-}
